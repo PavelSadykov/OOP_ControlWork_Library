4b6e9f02de59cfb7c2f62b3a59b537a1868dd0ea
Pavel Sadykov <2439747@gmail.com>
2023-02-08 11:54:56 +0500

    Create Header.cpp

---
+++ Header.cpp
@@ -0,0 +1,17 @@
+//
+//  Header.cpp
+//  Проверочная работа ООП 31.01.23г.
+//
+//  Created by Павел on 31.01.2023.
+//
+
+#include "Header.hpp"
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <utility>//std::pair
+#include <unordered_map>
+#include <queue>
+#include <iostream>
